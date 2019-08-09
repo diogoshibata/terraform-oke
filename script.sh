dbc5279e45105828377d24297b4cdbffda63368b
diogoshibata <35370798+diogoshibata@users.noreply.github.com>
2019-08-09 15:12:40 -0300

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo mkdir -p ~/.oci
-sudo cp terraform-oke/userdata/oci_api_key.pem ~/.oci/oci_api_key.pem
-sudo cp terraform-oke/userdata/config ~/.oci/config
-sudo oci setup repair-file-permissions --file ~/.oci/oci_api_key.pem
-sudo oci setup repair-file-permissions --file ~/.oci/config
+mkdir -p ~/.oci
+cp terraform-oke/userdata/oci_api_key.pem ~/.oci/oci_api_key.pem
+cp terraform-oke/userdata/config ~/.oci/config
+oci setup repair-file-permissions --file ~/.oci/oci_api_key.pem
+oci setup repair-file-permissions --file ~/.oci/config
