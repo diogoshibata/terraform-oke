52ca4ffb782cb05d42a4bf3a3512e3147d0c6543
diogoshibata <35370798+diogoshibata@users.noreply.github.com>
2019-08-09 23:16:51 -0300

    Add files via upload

---
+++ kubescript.sh
@@ -0,0 +1,8 @@
+export KUBECONFIG=./mykubeconfig
+kubectl config view
+kubectl get nodes
+kubectl delete deployment atp2 --ignore-not-found=true
+kubectl apply -f atp2.yaml
+kubectl get services atp2
+kubectl get pods
+kubectl describe pods
