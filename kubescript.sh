15878b73358ceecfd823a6d1b58337f77b367d4d
diogoshibata <35370798+diogoshibata@users.noreply.github.com>
2019-08-09 23:23:34 -0300

    Restart

--- kubescript.sh
+++
@@ -1,8 +0,0 @@
-export KUBECONFIG=./mykubeconfig
-kubectl config view
-kubectl get nodes
-kubectl delete deployment atp2 --ignore-not-found=true
-kubectl apply -f atp2.yaml
-kubectl get services atp2
-kubectl get pods
-kubectl describe pods
--- outputs.tf
+++
@@ -1,8 +0,0 @@
-
-output "bastion_id" {
-  value = ["${oci_core_instance.bastion.id}"]
-}
-
-output "bastion_ip" {
-  value = ["${oci_core_instance.bastion.public_ip}"]
-}
